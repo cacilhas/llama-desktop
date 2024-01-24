bb8eb94290505787552d7cfb013ce54f24160a57
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-01-23 22:29:09 -0300

    replace ollama-rs for reqwest to get model names

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 slint::include_modules!();
 
 use eyre::*;
-use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
 use slint::{spawn_local, SharedString, VecModel};
 use std::{env, rc::Rc};
 use url::Url;
 
-static mut OLLAMA: Option<Ollama> = None;
+static mut HOST: Option<String> = None;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     unsafe {
-        OLLAMA = Some(get_ollama()?);
+        HOST = Some(get_ollama_host()?);
     }
+
     let models = get_models().await?;
     let current_model = select_current_model(&models)?;
 
@@ -36,17 +36,17 @@ async fn main() -> Result<()> {
         dbg!("Querying", &model, &prompt);
         spawn_local(async move {
             dbg!("Spawning Ollama request");
-            use tokio_stream::StreamExt;
-            let mut stream = unsafe { OLLAMA.clone().unwrap() }
-                .generate_stream(GenerationRequest::new(model, prompt))
-                .await
-                .unwrap();
-
-            while let Some(res) = stream.next().await {
-                for chunk in res.unwrap().iter() {
-                    ui.invoke_update_response(chunk.response.to_owned().into());
-                }
-            }
+            // use tokio_stream::StreamExt;
+            // let mut stream = unsafe { OLLAMA.clone().unwrap() }
+            //     .generate_stream(GenerationRequest::new(model, prompt))
+            //     .await
+            //     .unwrap();
+            //
+            // while let Some(res) = stream.next().await {
+            //     for chunk in res.unwrap().iter() {
+            //         ui.invoke_update_response(chunk.response.to_owned().into());
+            //     }
+            // }
 
             ui.invoke_response_done();
             dbg!("Response received");
@@ -59,43 +59,39 @@ async fn main() -> Result<()> {
 
 /*----------------------------------------------------------------------------*/
 
-fn get_ollama() -> Result<Ollama> {
-    let (host, port) = get_ollama_host()?;
-    dbg!("Trying to connect", &host, port);
-    Ok(Ollama::new(host, port))
-}
-
-fn get_ollama_host() -> Result<(String, u16)> {
+fn get_ollama_host() -> Result<String> {
     let uri = env::var("OLLAMA_HOST").unwrap_or("http://localhost:11434".to_owned());
     let uri = Url::parse(&uri)?;
-    Ok((
-        format!(
-            "{}://{}",
-            uri.scheme(),
-            uri.host_str()
-                .ok_or_eyre(format!("fail to parse {}", uri))?
-        ),
+    Ok(format!(
+        "{}://{}:{}",
+        uri.scheme(),
+        uri.host_str()
+            .ok_or_eyre(format!("fail to parse {}", uri))?,
         uri.port().unwrap_or(11434),
     ))
 }
 
 async fn get_models() -> Result<Vec<String>> {
-    unsafe {
-        let models = OLLAMA
-            .clone()
-            .ok_or_eyre("ollama not connected")?
-            .list_local_models()
-            .await?
-            .iter()
-            .map(|model| model.name.to_owned())
-            .collect::<Vec<_>>();
+    let uri = unsafe {
+        format!(
+            "{}/api/tags",
+            HOST.clone().ok_or_eyre("OLLAMA_HOST not loaded yet")?
+        )
+    };
+    let models = reqwest::get(uri)
+        .await?
+        .json::<ModelList>()
+        .await?
+        .models
+        .iter()
+        .map(|model| model.name.to_owned())
+        .collect::<Vec<_>>();
 
-        if models.is_empty() {
-            Err(eyre!("no model found"))
-        } else {
-            dbg!("Models received");
-            Ok(models)
-        }
+    if models.is_empty() {
+        Err(eyre!("no model found"))
+    } else {
+        dbg!("Models received");
+        Ok(models)
     }
 }
 
@@ -103,8 +99,6 @@ fn select_current_model(models: &Vec<String>) -> Result<String> {
     let mut current_model: Option<String> = None;
     for model in models.clone().iter() {
         if current_model.is_none() && model == "llama2" {
-            current_model = Some(model.to_owned());
-        } else if model.contains("phind-codellama") {
             current_model = Some(model.to_owned());
             break;
         }
@@ -116,4 +110,17 @@ fn select_current_model(models: &Vec<String>) -> Result<String> {
             .ok_or_eyre("no model found")?
             .to_owned(),
     ))
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+struct Model {
+    name: String,
+    modified_at: String,
+    size: u64,
+    digest: String,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+struct ModelList {
+    models: Vec<Model>,
 }
