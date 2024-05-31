5408ec795a9f58626f55350d84264d557dda27a4
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-05-31 14:06:57 -0300

    isolate font initialisation into a dedicated mod

---
+++ src/fonts.rs
@@ -0,0 +1,18 @@
+use eframe::egui::{self, FontDefinitions};
+
+pub fn initialize_fonts() -> FontDefinitions {
+    let font1 = egui::FontData::from_static(include_bytes!("assets/aclonica.ttf"));
+    let font2 = egui::FontData::from_static(include_bytes!("assets/bellota.ttf"));
+    let mut fonts = egui::FontDefinitions::default();
+    fonts.font_data.insert("arial".into(), font1);
+    fonts.font_data.insert("sans".into(), font2);
+    fonts
+        .families
+        .insert(egui::FontFamily::Name("arial".into()), vec!["arial".into()]);
+    fonts
+        .families
+        .get_mut(&egui::FontFamily::Proportional)
+        .unwrap()
+        .insert(0, "sans".into());
+    fonts
+}
--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,27 @@ extern crate eframe;
 extern crate egui_extras;
 extern crate image;
 
+mod fonts;
 mod protocol;
 mod ui;
 
+use crate::fonts::initialize_fonts;
+use crate::ui::LlammaApp;
 use eframe::egui;
 use std::error;
-use ui::LlammaApp;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let viewport = egui::ViewportBuilder::default()
         .with_title("Llama Desktop")
         .with_inner_size([800.0, 1200.0]);
 
+    let fonts = initialize_fonts();
     let options = eframe::NativeOptions {
         viewport,
         centered: true,
         run_and_return: false,
         ..Default::default()
     };
-
-    let font1 = egui::FontData::from_static(include_bytes!("assets/aclonica.ttf"));
-    let font2 = egui::FontData::from_static(include_bytes!("assets/bellota.ttf"));
-    let mut fonts = egui::FontDefinitions::default();
-    fonts.font_data.insert("arial".into(), font1);
-    fonts.font_data.insert("sans".into(), font2);
-    fonts
-        .families
-        .insert(egui::FontFamily::Name("arial".into()), vec!["arial".into()]);
-    fonts
-        .families
-        .get_mut(&egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "sans".into());
 
     eframe::run_native(
         "llama-desktop",
