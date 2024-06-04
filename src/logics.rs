6448c6b7da6078be8d781257d11aeeeebf4eed3e
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-06-04 19:42:20 -0300

    separate LlamaApp in aspects

--- src/logics.rs
+++ src/logics.rs
@@ -6,6 +6,7 @@ use crate::ollama;
 use crate::protocol::{Request, Response};
 use reqwest::header;
 use tokio::time;
+use toml::Table;
 
 #[derive(Debug)]
 pub struct State {
@@ -21,22 +22,29 @@ pub struct State {
 
 impl State {
     pub fn reset(&mut self) {
+        _eprintln!("RESETING STATE");
         self.input = "Why the sky is blue?".to_owned();
         self.output = String::new();
         self.retrieving = false;
         self.reload = true;
         self.context = Vec::new();
+        _dbg!(self);
     }
 }
 
 pub async fn send() {
+    _eprintln!("SENDING CONTENT");
+
     let context = STATE.read().context.clone();
     let context: Option<Vec<u16>> = if context.is_empty() {
         None
     } else {
         Some(context.iter().map(|e| *e as u16).collect())
     };
+    _dbg!(&context);
+
     let input = STATE.read().input.to_owned();
+    _dbg!(&input);
     STATE
         .write()
         .output
@@ -49,6 +57,7 @@ pub async fn send() {
         "Content-Type",
         header::HeaderValue::from_static("application/json"),
     );
+    _dbg!(&headers);
     let client = reqwest::Client::builder()
         .default_headers(headers)
         .build()
@@ -62,10 +71,12 @@ pub async fn send() {
             context,
         }
     };
+    _dbg!(&payload);
     let payload = serde_json::to_string(&payload).unwrap();
     let uri = ollama::path("/api/generate");
-    // TODO: make timeout configurable
+    _dbg!(&uri);
     let timeout = time::Duration::from_secs(TIMEOUTS[STATE.read().timeout_idx] as u64);
+    _dbg!(&timeout);
 
     match time::timeout(timeout, client.post(uri).body(payload).send()).await {
         Ok(Ok(mut response)) => {
@@ -74,9 +85,11 @@ pub async fn send() {
                 return;
             }
 
+            _dbg!(&response);
             'read: while let Ok(current) = time::timeout(timeout, response.chunk()).await {
                 match current {
                     Ok(Some(current)) => {
+                        _dbg!(&current);
                         let chunk: Response =
                             serde_json::from_str(std::str::from_utf8(current.borrow()).unwrap())
                                 .unwrap();
@@ -86,6 +99,7 @@ pub async fn send() {
                             state.context = context.iter().map(|e| *e as i32).collect::<Vec<i32>>();
                         }
                         if chunk.done {
+                            _eprintln!("DONE");
                             break 'read;
                         }
                     }
@@ -125,13 +139,14 @@ fn timeout_with_error(err: impl Error) {
 }
 
 fn fail(message: impl Into<String> + Clone) {
-    let res = format!("\n\n## ERROR\n{}\n", message.clone().into());
+    _eprintln!("{}", message.clone().into());
+    let res = format!("\n\n## ERROR\n{}\n", message.into());
     STATE.write().output.push_str(&res);
-    eprintln!("{}", message.into());
     finish();
 }
 
 fn finish() {
+    _eprintln!("FINISHED");
     let mut state = STATE.write();
     state.output.push_str(HR);
     state.retrieving = false;
@@ -151,3 +166,11 @@ pub static mut STATE: State = State {
 };
 
 pub static TIMEOUTS: [usize; 7] = [10, 20, 30, 60, 120, 180, 300];
+
+#[dynamic]
+pub static VERSION: String = {
+    let cargo = include_str!("../Cargo.toml").parse::<Table>().unwrap();
+    let package = cargo["package"].as_table().unwrap();
+    let version = package["version"].as_str().unwrap();
+    return version.to_string();
+};
--- src/ui/app_impl.rs
+++ src/ui/app_impl.rs
@@ -1,118 +1,15 @@
-use std::{process, thread, time::Duration};
+use std::process;
 
+use super::BoxLayout;
+use super::RUNTIME;
 use crate::fonts::set_font_size;
 use crate::logics::*;
-use crate::ollama;
 use eframe::Frame;
 use eframe::*;
 use egui::*;
 use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
-use egui_extras::install_image_loaders;
-use tokio::runtime::Runtime;
-use tokio::sync::oneshot;
-use toml::Table;
 
-#[derive(Debug)]
-pub struct LlamaApp {
-    logo: ImageSource<'static>,
-    horizontal: ImageSource<'static>,
-    vertical: ImageSource<'static>,
-    title_font: FontId,
-    small_font: FontId,
-    box_layout: BoxLayout,
-}
-
-#[derive(Debug, Default, Eq, PartialEq)]
-enum BoxLayout {
-    Horizontally,
-    Vertically,
-    #[default]
-    NotSet,
-}
-
-/// LlamaApp is just a proxy for a module
-impl LlamaApp {
-    pub fn new(cc: &CreationContext<'_>, fonts: FontDefinitions) -> Self {
-        install_image_loaders(&cc.egui_ctx);
-        cc.egui_ctx.set_fonts(fonts);
-
-        let (inp, mut out) = oneshot::channel::<Vec<String>>();
-        RUNTIME.spawn(async move {
-            inp.send(ollama::get_models().await).unwrap();
-        });
-        STATE.write().models = loop {
-            match out.try_recv() {
-                Ok(cache) => break cache,
-                Err(oneshot::error::TryRecvError::Empty) => {
-                    thread::sleep(Duration::from_millis(100));
-                }
-                Err(err) => {
-                    eprintln!("failed to retrieve models: {}", err);
-                    panic!("failed retrieving models");
-                }
-            }
-        };
-
-        Self {
-            logo: include_image!("assets/logo.png"),
-            horizontal: include_image!("assets/horizontal.png"),
-            vertical: include_image!("assets/vertical.png"),
-            title_font: FontId::new(32.0, FontFamily::Name("arial".into())),
-            small_font: FontId::new(12.0, FontFamily::Name("arial".into())),
-            box_layout: BoxLayout::default(),
-        }
-    }
-
-    fn setup(&mut self, frame: &mut Frame) {
-        if let Some(storage) = frame.storage() {
-            self.setup_model(storage);
-            self.setup_timeout(storage);
-            self.setup_layout(storage);
-        } else {
-            let mut state = STATE.write();
-            state.selected_model = 0;
-            state.timeout_idx = 1;
-            self.box_layout = BoxLayout::Vertically;
-        }
-    }
-
-    fn setup_model(&mut self, storage: &dyn Storage) {
-        if STATE.read().selected_model > STATE.read().models.len() {
-            let selected_model = storage
-                .get_string("selected-model")
-                .unwrap_or("0".to_string());
-            STATE.write().selected_model = selected_model.parse().unwrap_or(0);
-        }
-    }
-
-    fn setup_timeout(&mut self, storage: &dyn Storage) {
-        if STATE.read().timeout_idx > TIMEOUTS.len() {
-            let timeout: usize = storage
-                .get_string("timeout")
-                .unwrap_or("20".to_string())
-                .parse()
-                .unwrap_or(20);
-            for (idx, tm) in TIMEOUTS.iter().enumerate() {
-                if *tm == timeout {
-                    STATE.write().timeout_idx = idx;
-                    break;
-                }
-            }
-        }
-    }
-
-    fn setup_layout(&mut self, storage: &dyn Storage) {
-        if self.box_layout == BoxLayout::NotSet {
-            if storage.get_string("layout").unwrap_or("V".to_string()) == "H".to_string() {
-                self.box_layout = BoxLayout::Horizontally;
-            } else {
-                self.box_layout = BoxLayout::Vertically;
-            }
-        }
-    }
-}
-
-impl App for LlamaApp {
+impl App for super::LlamaApp {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         ctx.set_visuals(Visuals::dark());
 
@@ -375,15 +272,4 @@ impl App for LlamaApp {
 }
 
 #[dynamic]
-static RUNTIME: Runtime = Runtime::new().unwrap();
-
-#[dynamic]
 static mut MD_CACHE: CommonMarkCache = CommonMarkCache::default();
-
-#[dynamic]
-static VERSION: String = {
-    let cargo = include_str!("../Cargo.toml").parse::<Table>().unwrap();
-    let package = cargo["package"].as_table().unwrap();
-    let version = package["version"].as_str().unwrap();
-    return version.to_string();
-};
