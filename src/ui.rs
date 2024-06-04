f72ed71add76bff9a310a95155cf9858453da037
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-06-04 18:55:27 -0300

    make unit explicit

--- src/ui.rs
+++ src/ui.rs
@@ -252,11 +252,12 @@ impl App for LlamaApp {
                     uis[8].with_layout(Layout::right_to_left(Align::Min), |ui| {
                         let mut state = STATE.write();
                         ComboBox::from_label(RichText::new("Timeout:").strong())
-                            .selected_text(format!("{}", TIMEOUTS[state.timeout_idx]))
+                            .selected_text(format!("{}s", TIMEOUTS[state.timeout_idx]))
                             .show_ui(ui, |ui| {
                                 let mut idx = state.timeout_idx;
                                 for (i, tm) in TIMEOUTS.iter().enumerate() {
-                                    let value = ui.selectable_value(&mut idx, i, format!("{}", tm));
+                                    let value =
+                                        ui.selectable_value(&mut idx, i, format!("{}s", tm));
                                     if value.clicked() {
                                         idx = i;
                                     }
