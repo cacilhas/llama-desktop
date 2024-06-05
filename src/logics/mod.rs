db2cbd8c99a93bc1b50f8077eeecdfd7b34070bd
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-06-05 12:22:20 -0300

    segment code

---
+++ src/logics/mod.rs
@@ -0,0 +1,9 @@
+mod sender;
+mod state;
+mod timeouts;
+mod version;
+
+pub use self::sender::*;
+pub use self::state::*;
+pub use self::timeouts::*;
+pub use self::version::*;
---
+++ src/logics/sender.rs
@@ -0,0 +1,114 @@
+use std::borrow::Borrow;
+
+use super::state::STATE;
+use super::timeouts::TIMEOUTS;
+use crate::helpers::{format_input_to_output, HR};
+use crate::ollama;
+use crate::protocol::{Request, Response};
+use eyre::{eyre, Result};
+use reqwest::header;
+use tokio::time;
+
+#[derive(Debug, Default)]
+pub struct Sender;
+
+impl Drop for Sender {
+    fn drop(&mut self) {
+        _eprintln!("FINISHED");
+        let mut state = STATE.write();
+        state.output.push_str(HR);
+        state.retrieving = false;
+        state.escape = false;
+        state.reload = true;
+    }
+}
+
+impl Sender {
+    pub async fn send(self) {
+        if let Err(err) = self.do_send().await {
+            let mut state = STATE.write();
+            _eprintln!("{:?}", err);
+            state.output.push_str("\n## ERROR:\n");
+            state.output.push_str(&format!("{}", err));
+        }
+    }
+
+    async fn do_send(&self) -> Result<()> {
+        _eprintln!("SENDING CONTENT");
+
+        let context = STATE.read().context.clone();
+        let context: Option<Vec<u16>> = if context.is_empty() {
+            None
+        } else {
+            Some(context.iter().map(|e| *e as u16).collect())
+        };
+        _dbg!(&context);
+
+        let input = STATE.read().input.to_owned();
+        _dbg!(&input);
+        STATE
+            .write()
+            .output
+            .push_str(&format_input_to_output(input.clone()));
+        STATE.write().output.push_str("\n\n");
+        STATE.write().input.clear();
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            "Content-Type",
+            header::HeaderValue::from_static("application/json"),
+        );
+        _dbg!(&headers);
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()?;
+        let payload = {
+            let state = STATE.read();
+            Request {
+                model: state.models[state.selected_model].to_owned(),
+                prompt: input,
+                stream: true,
+                context,
+            }
+        };
+        _dbg!(&payload);
+        let payload = serde_json::to_string(&payload)?;
+        let uri = ollama::path("/api/generate");
+        _dbg!(&uri);
+        let timeout = time::Duration::from_secs(TIMEOUTS[STATE.read().timeout_idx] as u64);
+        _dbg!(&timeout);
+
+        self.check_escape()?;
+        let mut response = time::timeout(timeout, client.post(uri).body(payload).send()).await??;
+        if !response.status().is_success() {
+            return Err(eyre![response.text().await?]);
+        }
+
+        _dbg!(&response);
+        self.check_escape()?;
+        'read: while let Some(current) = time::timeout(timeout, response.chunk()).await?? {
+            _dbg!(&current);
+            self.check_escape()?;
+            let chunk: Response = serde_json::from_str(std::str::from_utf8(current.borrow())?)?;
+            let mut state = STATE.write();
+            state.output.push_str(&chunk.response);
+            if let Some(context) = chunk.context {
+                state.context = context.iter().map(|e| *e as i32).collect::<Vec<i32>>();
+            }
+            if chunk.done {
+                break 'read;
+            }
+        }
+
+        _eprintln!("DONE");
+        Ok(())
+    }
+
+    fn check_escape(&self) -> Result<()> {
+        if STATE.read().escape {
+            Err(eyre::eyre!["Escape key pressed."])
+        } else {
+            Ok(())
+        }
+    }
+}
